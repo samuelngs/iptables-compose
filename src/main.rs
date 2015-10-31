354539b00434db2de8e183217243a71b0fcc45da
Samuel <sng@storyair.com>
2015-10-31 17:04:29 -0600

    Added basic command-line usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+
+// YAML files as iptables configuration sources
+
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
+    let matches = App::new("iptables-init")
+        .version("1.0.0")
+        .author("Samuel. <sam@infinitely.io>")
+        .arg(Arg::with_name("CONFIG")
+             .short("c")
+             .long("config")
+             .help("Yaml file as iptables configuration source")
+             .takes_value(true))
+        .get_matches();
+
     println!("YAML files as iptables configuration sources");
+
+    let config = matches.value_of("CONFIG").unwrap_or("");
+    println!("Value for config: {}", config);
 }
